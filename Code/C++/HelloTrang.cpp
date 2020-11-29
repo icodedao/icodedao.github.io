b567eff5c78f9062cae015f7b7fee8e27c4f73aa
thanhphongla <thanhphongtran.it@gmail.com>
2020-11-29 16:12:58 +0700

    1.20

--- Code/C++/HelloTrang.cpp
+++ Code/C++/HelloTrang.cpp
@@ -1,7 +1,38 @@
-#include<iostream> 
-int main()
-{
-	int n; 
-	std::cout<<"Hello Pham Le Huyen Trang\n" ; 
-	return 0;
+#include<iostream>
+using namespace std;
+int main () {
+	int h;
+	cout << "Enter H = ";
+	cin >> h;
+	for ( int i=1 ; i<=h ; i++) {
+		for ( int j=1 ; j<= 2*h ; j++) {
+			if ( j >= i && j<= h ) {
+				cout <<"* ";
+			}
+			else if (j > 2*h-i ) {
+				cout <<"* ";
+			}
+			else {
+				cout <<"  ";
+			}
+		}
+	cout <<"\n";
+		
+	}
+	for ( int i=1 ; i<=h ; i++) {
+		for ( int j=1 ; j<= 2*h ; j++) {
+			if ( j<= h-i+1) {
+				cout <<"* ";
+			}
+			else if ( j>h && j <= 2*h-(h-i) ) {
+				cout <<"* ";
+			}
+			else {
+				cout <<"  ";
+			}
+	
+		}
+	cout <<"\n";
+	}
+	
 }
--- HKI 2020 2021/LTM/Data/Lap Trinh Mang/src/Tuan10/Server.java
+++ HKI 2020 2021/LTM/Data/Lap Trinh Mang/src/Tuan10/Server.java
@@ -8,31 +8,32 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-	public static int port = 1234;
-	public static int numThread = 2;
-	private static ServerSocket server = null;
-	public static Vector<Worker> workers = new Vector<>();
-        public static int i = 0;
 
-	public static void main(String[] args) throws IOException {
-		int i = 0;
-		ExecutorService executor = Executors.newFixedThreadPool(numThread);
-		try {
-			server = new ServerSocket(port);
-			System.out.println("Server binding at port " + port);
-			System.out.println("Waiting for client...");
-			while(true) {
-				i++;
-				Socket socket = server.accept();
-				Worker client = new Worker(socket, Integer.toString(i));
-				workers.add(client);
-				executor.execute(client);
-			}
-		} catch (IOException e) {
-			System.out.println(e);
-		} finally {
-			if(server!=null)
-				server.close();
-		}
-	}
+    public static int port = 1234;
+    public static int numThread = 3;
+    private static ServerSocket server = null;
+    public static Vector<Worker> workers = new Vector<>();
+    public static int i = 0;
+
+    public static void main(String[] args) throws IOException {
+        ExecutorService executor = Executors.newFixedThreadPool(numThread);
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server binding at port " + port);
+            System.out.println("Waiting for client...");
+            while (true) {
+                i++;
+                Socket socket = server.accept();
+                Worker client = new Worker(socket, Integer.toString(i));
+                workers.add(client);
+                executor.execute(client);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (server != null) {
+                server.close();
+            }
+        }
+    }
 }
